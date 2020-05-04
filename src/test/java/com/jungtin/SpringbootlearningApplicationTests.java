964a1f7f3f83755f42320b7dab2a636660fb4c1d
jungtin <nguyentrungthinh2000@gmail.com>
2020-05-04 10:40:06 +0700

    create springboot

---
+++ src/test/java/com/jungtin/SpringbootlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungtin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootlearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
